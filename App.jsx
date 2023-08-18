5f907135a24d8556eb9f61eb41fe197a66b6669e
Vikyath Jain B A <142490182+vikyathjain12@users.noreply.github.com>
2023-08-18 09:44:21 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import reactLogo from './assets/react.svg'
+import './App.css';
+import Login from  "./components/Login"
+
+
+function App() {  
+  
+
+  return (
+      <div Classname="read-the-docs ">
+        {/* <h2>Reels APP🎶🎶</h2> */}
+         <Login></Login>
+         {/*Login Page*/}
+
+      </div>
+
+  ) 
+}
+
+export default App
